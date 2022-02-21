6d3061f8c6be002f29db2c81f1f56d62fb8ed954
Kristina Ackovska <kristinaackovska88@gmail.com>
2022-02-21 22:44:31 +0100

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        sayhello("Kristina");
+        sayGoodBye("Kristina");
+
+
+    }
+
+
+
+    public int getAge() {
+        return age;
+
+
+
+
+
+    }
+
+
+
+
+    public HelloWorld(int age) {
+        this.age = age;
+        sayhello("Kristina");
+
+    }
+
+	boolean flag = true;
+            if(flag == true) {
+                System.out.println("Value of flag is true");
+            } else {
+                System.out.println("Value of flag is false");
+            }
+
+    private int age = 5;
+
+    public static void sayhello (String name){
+        System.out.println("Hello " +name);
+
+        // TODO: to be executed one BUG-001 id fixed
+
+    }
+
+    // Thid method will print Good bye message
+    /*
+
+
+
+
+
+
+     */
+    public static void sayGoodBye (String name){
+        //TODO:  some additional validation to be added
+
+        System.out.println("Good bye " + name + " test");
+        String messages = "hello";
+    }
+
+
+
+
+}
